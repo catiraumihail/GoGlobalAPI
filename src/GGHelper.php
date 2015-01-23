a6c922c4faafc90975570befb49246a13046490b
Catirau Mihail <catiraumihail@gmail.com>
2015-01-23 13:56:04 +0200

    Create GGHelper.php

---
+++ src/GGHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Common functions */
+
+namespace GoGlobalApi\lib;
+
+class GGHelper{
+    public static function WrapTag($tag, $value, $attributes=array()) {
+        $attr = "";
+        foreach($attributes as $k=>$v) $attr.= sprintf(" %s=\"%s\"",$k,$v);
+		if(!empty($value))
+			return sprintf("<%s%s>%s</%s>", $tag, $attr, $value, $tag);
+		else
+			return sprintf("<%s%s/>",$tag,$attr);
+    }
+}
